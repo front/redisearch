f0540df294ee5c3232e3e3d736f61e97574b4a59
Fábio Neves <eu@fabioneves.com>
2019-02-13 22:24:48 +0000

    Add 'FT.INFO' with getInfo() method

--- src/Index.php
+++ src/Index.php
@@ -15,303 +15,347 @@ class Index {
 	/**
 	 * @var object
 	 */
-  public $client;
+	public $client;
 
 	/**
 	 * @var object
 	 */
-  private $index;
+	private $index;
 
 	/**
 	 * @var string
 	 */
-  private $indexName;
+	private $indexName;
 
-  /** 
-   * @var bool 
-   */
-  private $noOffsetsEnabled = false;
-  
-  /** 
-   * @var bool 
-   */
-  private $noFieldsEnabled = false;
-      
-  /** 
-   * @var array|int
-   */
-  private $stopWords = null;
+	/**
+	 * @var bool
+	 */
+	private $noOffsetsEnabled = FALSE;
 
-  public function __construct( $client ) {
-    $this->client = $client;
-  }
+	/**
+	 * @var bool
+	 */
+	private $noFieldsEnabled = FALSE;
 
-  /**
-  * Drop existing index.
-  * @param
-  * @return
-  */
-  public function drop() {
-    return $this->client->rawCommand( 'FT.DROP', array( $this->getIndexName() ) );
-  }
+	/**
+	 * @var array|int
+	 */
+	private $stopWords = NULL;
 
-  /**
-  * Create index in redis server from raw params.
-  * @since    0.1.0
-  * @param 
-  * @return
-  */
-  public function rawCreate( $indexName = null, $schema = array(), $stop_words = null ) {
-    if ( !isset( $indexName ) ) {
-      return;
-    }
+	public function __construct( $client ) {
+		$this->client = $client;
+	}
 
-    if ( empty( $schema ) ) {
-      return;
-    }
+	/**
+	 * Drop existing index.
+	 *
+	 * @param
+	 *
+	 * @return
+	 */
+	public function drop() {
+		return $this->client->rawCommand( 'FT.DROP', array( $this->getIndexName() ) );
+	}
 
-    $index_schema = array( $indexName );
+	/**
+	 * Create index in redis server from raw params.
+	 *
+	 * @since    0.1.0
+	 *
+	 * @param
+	 *
+	 * @return
+	 */
+	public function rawCreate( $indexName = NULL, $schema = array(), $stop_words = NULL ) {
+		if ( ! isset( $indexName ) ) {
+			return;
+		}
 
-    if ( $stop_words == 0 ) {
-      $index_schema = array_merge( $index_schema, array( 'STOPWORDS', 0 ) );
-    } else if ( is_array( $stop_words ) && !empty( $stop_words ) ) {
-      $stop_words = array_map( 'trim', $stop_words );
-      $stop_words_count = count( $stop_words );
-      if ( $stop_words_count != 0 ) {
-        $index_schema = array_merge( $index_schema, array( 'STOPWORDS', $stop_words_count ), $stop_words );
-      }
-    }
+		if ( empty( $schema ) ) {
+			return;
+		}
 
-    $index_schema = array_merge( $index_schema, array( 'SCHEMA' ), $schema );
+		$index_schema = array( $indexName );
 
-    return $this->client->rawCommand('FT.CREATE', $index_schema);
-  }
+		if ( $stop_words == 0 ) {
+			$index_schema = array_merge( $index_schema, array( 'STOPWORDS', 0 ) );
+		} else if ( is_array( $stop_words ) && ! empty( $stop_words ) ) {
+			$stop_words       = array_map( 'trim', $stop_words );
+			$stop_words_count = count( $stop_words );
+			if ( $stop_words_count != 0 ) {
+				$index_schema = array_merge( $index_schema, array( 'STOPWORDS', $stop_words_count ), $stop_words );
+			}
+		}
 
-  /**
-   * Create index with passed fields and settings
-   * @return mixed
-   */
-  public function create() {
-    $properties = array( $this->getIndexName() );
+		$index_schema = array_merge( $index_schema, array( 'SCHEMA' ), $schema );
 
-    if ( $this->isNoOffsetsEnabled() ) {
-      $properties[] = 'NOOFFSETS';
-    }
+		return $this->client->rawCommand( 'FT.CREATE', $index_schema );
+	}
 
-    if ( $this->isNoFieldsEnabled() ) {
-      $properties[] = 'NOFIELDS';
-    }
+	/**
+	 * Create index with passed fields and settings
+	 *
+	 * @return mixed
+	 */
+	public function create() {
+		$properties = array( $this->getIndexName() );
 
-    if ( !is_null( $this->stopWords ) ) {
-      $properties[] = 'STOPWORDS';
-      $properties[] = count( $this->stopWords );
-      $properties = array_merge( $properties, $this->stopWords );
-    }
+		if ( $this->isNoOffsetsEnabled() ) {
+			$properties[] = 'NOOFFSETS';
+		}
 
-    if ( $this->stopWords == 0 ) {
-      $properties[] = 'STOPWORDS';
-      $properties[] = 0;
-    }
+		if ( $this->isNoFieldsEnabled() ) {
+			$properties[] = 'NOFIELDS';
+		}
 
-    $properties[] = 'SCHEMA';
+		if ( ! is_null( $this->stopWords ) ) {
+			$properties[] = 'STOPWORDS';
+			$properties[] = count( $this->stopWords );
+			$properties   = array_merge( $properties, $this->stopWords );
+		}
 
-    $fieldDefinitions = [];
-    foreach (get_object_vars($this) as $field) {
-      if ( $field instanceof FieldInterface ) {
-        $fieldDefinitions = array_merge($fieldDefinitions, $field->getDefinition());
-      }
-    }
+		if ( $this->stopWords == 0 ) {
+			$properties[] = 'STOPWORDS';
+			$properties[] = 0;
+		}
 
-    if (count($fieldDefinitions) === 0) {
-      return $this;
-    }
+		$properties[] = 'SCHEMA';
 
-    return $this->client->rawCommand('FT.CREATE', array_merge($properties, $fieldDefinitions));
-  }
-  
+		$fieldDefinitions = [];
+		foreach ( get_object_vars( $this ) as $field ) {
+			if ( $field instanceof FieldInterface ) {
+				$fieldDefinitions = array_merge( $fieldDefinitions, $field->getDefinition() );
+			}
+		}
 
-  /**
-   * @return bool
-   */
-  public function isNoOffsetsEnabled() {
-    return $this->noOffsetsEnabled;
-  }
+		if ( count( $fieldDefinitions ) === 0 ) {
+			return $this;
+		}
 
-  /**
-   * @param bool $noOffsetsEnabled
-   * @return IndexInterface
-   */
-  public function setNoOffsetsEnabled( $noOffsetsEnabled ) {
-    $this->noOffsetsEnabled = $noOffsetsEnabled;
-    return $this;
-  }
+		return $this->client->rawCommand( 'FT.CREATE', array_merge( $properties, $fieldDefinitions ) );
+	}
 
 
-  /**
-   * @return bool
-   */
-  public function isNoFieldsEnabled() {
-    return $this->noFieldsEnabled;
-  }
+	/**
+	 * @return bool
+	 */
+	public function isNoOffsetsEnabled() {
+		return $this->noOffsetsEnabled;
+	}
 
-  /**
-   * @param bool $noFieldsEnabled
-   * @return IndexInterface
-   */
-  public function setNoFieldsEnabled( $noFieldsEnabled ) {
-    $this->noFieldsEnabled = $noFieldsEnabled;
-    return $this;
-  }
+	/**
+	 * @param bool $noOffsetsEnabled
+	 *
+	 * @return IndexInterface
+	 */
+	public function setNoOffsetsEnabled( $noOffsetsEnabled ) {
+		$this->noOffsetsEnabled = $noOffsetsEnabled;
+
+		return $this;
+	}
 
 
-  /**
-   * @param string $indexName
-   * @return string
-   */
-  public function setIndexName( $indexName ) {
-    $this->indexName = $indexName;
-    return $this;
-  }
-  
-  /**
-   * @return string
-   */
-  public function getIndexName() {
-    return !is_string($this->indexName) || $this->indexName === '' ? self::class : $this->indexName;
-  }
+	/**
+	 * @return bool
+	 */
+	public function isNoFieldsEnabled() {
+		return $this->noFieldsEnabled;
+	}
 
-  /**
-   * @param array   $stopWords    Array of custom stop words
-   * @return array
-   */
-  public function setStopWords( $stopWords = null ) {
-    $this->stopWords = $stopWords;
-    return $this;
-  }
+	/**
+	 * @param bool $noFieldsEnabled
+	 *
+	 * @return IndexInterface
+	 */
+	public function setNoFieldsEnabled( $noFieldsEnabled ) {
+		$this->noFieldsEnabled = $noFieldsEnabled;
 
-  /**
-   * @return int
-   */
-  public function noStopWords() {
-    $this->stopWords = 0;
-    return $this;
-  }
+		return $this;
+	}
 
-  /**
-   * @return int
-   */
-  public function synonymAdd( $synonymList = array() ) {
-    if ( empty( $synonymList ) || !is_array( $synonymList ) ) {
-      return;
-    }
 
-    foreach ($synonymList as $synonym) {
-      $synonymGroup = array_map( 'trim', $synonym );
-      $synonymCommand = array_merge( array( $this->getIndexName() ), $synonymGroup );
+	/**
+	 * @param string $indexName
+	 *
+	 * @return string
+	 */
+	public function setIndexName( $indexName ) {
+		$this->indexName = $indexName;
 
-      $this->client->rawCommand('FT.SYNADD', $synonymCommand);
-    }
-  }
+		return $this;
+	}
 
-  /**
-   * @return array
-   */
-  protected function getFields() {
-    $fields = [];
-    foreach (get_object_vars($this) as $field) {
-      if ($field instanceof FieldInterface) {
-        $fields[$field->getName()] = $field;
-      }
-    }
+	/**
+	 * @return string
+	 */
+	public function getIndexName() {
+		return ! is_string( $this->indexName ) || $this->indexName === '' ? self::class : $this->indexName;
+	}
 
-    return $fields;
-  }
+	/**
+	 * @param array $stopWords Array of custom stop words
+	 *
+	 * @return array
+	 */
+	public function setStopWords( $stopWords = NULL ) {
+		$this->stopWords = $stopWords;
 
-  /**
-  * Add documents to the index.
-  * @since    0.1.0
-  * @param
-  * @return object $this
-  */
-  public function add( $document ) {
-    $properties = $document->getDefinition();
-    array_unshift( $properties, $this->getIndexName() );
+		return $this;
+	}
 
-    return $this->client->rawCommand( 'FT.ADD', $properties );
-  }
+	/**
+	 * @return int
+	 */
+	public function noStopWords() {
+		$this->stopWords = 0;
 
-    /**
-   * @param string $name
-   * @param float $weight
-   * @param bool $sortable
-   * @param bool $noindex
-   * @return IndexInterface
-   */
-  public function addTextField( $name, $weight = 1.0, $sortable = false, $noindex = false) {
-    $this->$name = (new TextField($name))->setSortable($sortable)->setNoindex($noindex)->setWeight($weight);
-    return $this;
-  }
+		return $this;
+	}
 
-    /**
-   * @param string $name
-   * @param float $weight
-   * @param bool $sortable
-   * @param bool $noindex
-   * @return IndexInterface
-   */
-  public function addTagField( $name, $sortable = false, $noindex = false, $separator = ',') {
-    $this->$name = (new TagField($name))->setSortable($sortable)->setNoindex($noindex)->setSeparator($separator);
-    return $this;
-  }
+	/**
+	 * @return int
+	 */
+	public function synonymAdd( $synonymList = array() ) {
+		if ( empty( $synonymList ) || ! is_array( $synonymList ) ) {
+			return;
+		}
 
-      /**
-   * @param string $name
-   * @param bool $sortable
-   * @param bool $noindex
-   * @return IndexInterface
-   */
-  public function addNumericField( $name, $sortable = false, $noindex = false) {
-      $this->$name = (new NumericField($name))->setSortable($sortable)->setNoindex($noindex);
-      return $this;
-  }
+		foreach ( $synonymList as $synonym ) {
+			$synonymGroup   = array_map( 'trim', $synonym );
+			$synonymCommand = array_merge( array( $this->getIndexName() ), $synonymGroup );
 
-  /**
-   * @param string $name
-   * @return IndexInterface
-   */
-  public function addGeoField( $name, $noindex = false ) {
-      $this->$name = (new GeoField($name))->setNoindex($noindex);
-      return $this;
-  }
+			$this->client->rawCommand( 'FT.SYNADD', $synonymCommand );
+		}
+	}
 
-  /**
-  * Delete post from index.
-  * @since    0.1.0
-  * @param
-  * @return object $this
-  */
-  public function delete($indexName = null, $id = 0) {
-    if ( !isset( $indexName ) ) {
-      return;
-    }
+	/**
+	 * @return array
+	 */
+	protected function getFields() {
+		$fields = [];
+		foreach ( get_object_vars( $this ) as $field ) {
+			if ( $field instanceof FieldInterface ) {
+				$fields[ $field->getName() ] = $field;
+			}
+		}
 
-    if ( $id == 0 ) {
-      return;
-    }
+		return $fields;
+	}
 
-    $command = array( $indexName, $id, 'DD' );
-    $this->client->rawCommand('FT.DEL', $command);
-    return $this;
-  }
+	/**
+	 * Add documents to the index.
+	 *
+	 * @since    0.1.0
+	 *
+	 * @param
+	 *
+	 * @return object $this
+	 */
+	public function add( $document ) {
+		$properties = $document->getDefinition();
+		array_unshift( $properties, $this->getIndexName() );
 
-  /**
-  * Write entire redisearch index to the disk to persist it.
-  * @since    0.1.0
-  * @param
-  * @return
-  */
-  public function writeToDisk() {
-    return $this->client->rawCommand('SAVE', []);
-  }
+		return $this->client->rawCommand( 'FT.ADD', $properties );
+	}
+
+	/**
+	 * @param string $name
+	 * @param float  $weight
+	 * @param bool   $sortable
+	 * @param bool   $noindex
+	 *
+	 * @return IndexInterface
+	 */
+	public function addTextField( $name, $weight = 1.0, $sortable = FALSE, $noindex = FALSE ) {
+		$this->$name = ( new TextField( $name ) )->setSortable( $sortable )->setNoindex( $noindex )->setWeight( $weight );
+
+		return $this;
+	}
+
+	/**
+	 * @param string $name
+	 * @param float  $weight
+	 * @param bool   $sortable
+	 * @param bool   $noindex
+	 *
+	 * @return IndexInterface
+	 */
+	public function addTagField( $name, $sortable = FALSE, $noindex = FALSE, $separator = ',' ) {
+		$this->$name = ( new TagField( $name ) )->setSortable( $sortable )->setNoindex( $noindex )->setSeparator( $separator );
+
+		return $this;
+	}
+
+	/**
+	 * @param string $name
+	 * @param bool   $sortable
+	 * @param bool   $noindex
+	 *
+	 * @return IndexInterface
+	 */
+	public function addNumericField( $name, $sortable = FALSE, $noindex = FALSE ) {
+		$this->$name = ( new NumericField( $name ) )->setSortable( $sortable )->setNoindex( $noindex );
+
+		return $this;
+	}
+
+	/**
+	 * @param string $name
+	 *
+	 * @return IndexInterface
+	 */
+	public function addGeoField( $name, $noindex = FALSE ) {
+		$this->$name = ( new GeoField( $name ) )->setNoindex( $noindex );
+
+		return $this;
+	}
+
+	/**
+	 * Delete post from index.
+	 *
+	 * @since    0.1.0
+	 *
+	 * @param
+	 *
+	 * @return object $this
+	 */
+	public function delete( $indexName = NULL, $id = 0 ) {
+		if ( ! isset( $indexName ) ) {
+			return;
+		}
+
+		if ( $id == 0 ) {
+			return;
+		}
+
+		$command = array( $indexName, $id, 'DD' );
+		$this->client->rawCommand( 'FT.DEL', $command );
+
+		return $this;
+	}
+
+	/**
+	 * Write entire redisearch index to the disk to persist it.
+	 *
+	 * @since    0.1.0
+	 *
+	 * @param
+	 *
+	 * @return
+	 */
+	public function writeToDisk() {
+		return $this->client->rawCommand( 'SAVE', [] );
+	}
+
+	/**
+	 * Returns information and statistics on the index.
+	 *
+	 * @param null $indexName
+	 *
+	 * @return mixed
+	 */
+	public function getInfo( $indexName = NULL ) {
+		return $this->client->rawCommand( 'FT.INFO', [ empty( $indexName ) ? $this->indexName : $indexName ] );
+	}
 
 }
