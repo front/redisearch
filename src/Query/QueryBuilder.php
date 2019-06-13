58fca2545c5822a5ad29a6f9d0cdf903ff5e21ac
bjarzyna <bjarzyna.dev@gmail.com>
2019-06-13 11:25:08 +0200

    QueryBuilder improvements and documentation

--- src/Query/QueryBuilder.php
+++ src/Query/QueryBuilder.php
@@ -12,12 +12,46 @@ use InvalidArgumentException;
  */
 class QueryBuilder {
 
+  /**
+   * @var string[] conditions not assigned to fields (fulltext matching)
+   */
   protected $genericConditions = [];
+
+  /**
+   * @var string[] conditions assigned to specific fields
+   */
   protected $conditions = [];
+
+  /**
+   * @var string operator which will join all conditions (AND or OR)
+   */
   protected $conjunction;
+
+  /**
+   * @var boolean should empty search terms be replaced with a wildcard
+   */
   protected $allOnEmpty;
+
+  /**
+   * @var boolean should search terms be split into words
+   */
+  protected $tokenize;
+
+  /**
+   * @var boolean should string be searched using prefix matching
+   */
   protected $partialSearch;
 
+  /**
+   * @var integer should string be searched using fuzzy matching
+   */
+  protected $fuzzySearch;
+
+  /**
+   * Transforms conjunction operator into redisearch equivalent
+   * @param string $conjunction conjunction operator (AND or OR)
+   * @return string redisearch conjunctor
+   */
   protected function getConjunction($conjunction) {
     $available = [
       'AND' => ' ',
@@ -27,29 +61,87 @@ class QueryBuilder {
     return $available[$conjunction] ?? $available['AND'];
   }
 
+  /**
+   * Applies various search method to parameters, such as prefix matching
+   * @param string[] $values an array of search terms
+   * @return string[] an array of search terms with search methods applied
+   */
   protected function applySearchMethods(array $values) {
-    if ($this->partialSearch) {
-      foreach ($values as $i => $value) {
-        $values[$i] = "$value*";
+
+    if ($this->tokenize) {
+      $new_values = []; 
+      foreach ($values as $value) {
+        $new_values = array_merge($new_values, explode(' ', $value));
       }
+      $values = $new_values;
+    }
+
+    foreach ($values as $i => $value) {
+      $tmp = [];
+
+      if ($this->partialSearch) {
+        $tmp[] = "$value*";
+      }
+      elseif ($this->fuzzySearch && $this->fuzzySearch >= 1 && $this->fuzzySearch <= 3) {
+        $fuzzy_border = str_repeat('%', $this->fuzzySearch);
+        $tmp[] = "$fuzzy_border$value$fuzzy_border";
+      }
+
+      if (count($tmp) == 0) {
+        continue;
+      }
+
+      $value = implode($this->getConjunction('OR'), $tmp);
+      if (count($tmp) > 1) {
+        $value = "($value)";
+      }
+      $values[$i] = $value;
     }
 
     return $values;
   }
 
+  /**
+   * Creates a condition out of search terms and conjunction operator
+   * @param string[] $values array of search terms
+   * @param string|NULL $conjunction conjunction operator
+   * @return string created condition string
+   */
   protected function createCondition(array $values, $conjunction = NULL) {
     $conjunction = $conjunction ?? $this->conjunction;
     $condition = implode($this->getConjunction($conjunction), $values);
     return $condition;
   }
 
+  /**
+   * Constructs the object and sets parameters
+   * @param array $params list of parameters to search query. Following keys allowed:
+   * - conjunction
+   * - allOnEmpty
+   * - tokenize
+   * - partialSearch
+   * - fuzzySearch
+   */
   public function __construct($params) {
     $this->conjunction = $params['conjunction'] ?? 'AND';
     $this->allOnEmpty = $params['allOnEmpty'] ?? TRUE;
+    $this->tokenize = $params['tokenize'] ?? FALSE;
     $this->partialSearch = $params['partialSearch'] ?? FALSE;
+    $this->fuzzySearch = $params['fuzzySearch'] ?? FALSE;
   }
 
-  public function addCondition($field, array $values, $conjunction = 'AND') {
+  /**
+   * Adds condition based on specific field
+   * @param string $field field's name
+   * @param string[] $values list of search terms
+   * @param string $conjunction conjunction operator
+   * @param boolean $exact whenever to apply search methods on search terms, such as tokenization
+   * @return self
+   */
+  public function addCondition($field, array $values, $conjunction = 'AND', $exact = FALSE) {
+    if (!$exact) {
+      $values = $this->applySearchMethods($values);
+    }
     $condition = $this->createCondition($values, $conjunction);
     $this->conditions[$field] = $this->conditions[$field] ?? [];
     $this->conditions[$field][] = $condition;
@@ -57,20 +149,39 @@ class QueryBuilder {
     return $this;
   }
 
-  public function addGenericCondition(array $values, $conjunction = 'AND') {
-    $values = $this->applySearchMethods($values);
+  /**
+   * Adds fullsearch condition (not based on specific field)
+   * @param string[] $values list of search terms
+   * @param string $conjunction conjunction operator
+   * @param boolean $exact whenever to apply search methods on search terms, such as tokenization
+   * @return self
+   */
+  public function addGenericCondition(array $values, $conjunction = 'AND', $exact = FALSE) {
+    if (!$exact) {
+      $values = $this->applySearchMethods($values);
+    }
     $condition = $this->createCondition($values, $conjunction);
     $this->genericConditions[] = $condition;
 
     return $this;
   }
 
+  /**
+   * Adds subcondition
+   * @param QueryBuilder $subcondition a condition to be nested
+   * @return self
+   */
   public function addSubcondition(QueryBuilder $subcondition) {
     $this->genericConditions[] = $subcondition;
 
     return $this;
   }
 
+  /**
+   * Builds redisearch query string
+   * @param boolean $subquery whether this is nested build call
+   * @return string built query
+   */
   public function buildRedisearchQuery($subquery = FALSE) {
     $query_array = [];
 
@@ -109,6 +220,12 @@ class QueryBuilder {
     return $query;
   }
 
+  /**
+   * Creates QueryBuilder instance from ConditionGroupInterface instance
+   * @param ConditionGroupInterface $condition
+   * @param string[] $params a list of parameters passed to QueryBuilder constructor
+   * @return QueryBuilder
+   */
   public static function fromConditionGroup(ConditionGroupInterface $condition, array $params = []) {
     $params["conjunction"] = $condition->getConjunction();
     $object = new static($params);
@@ -126,7 +243,7 @@ class QueryBuilder {
 
         // filter as much as possible - discard unsupported operators
         if ($operator == '=') {
-          $object->addCondition($field, (array) $value);
+          $object->addCondition($field, (array) $value, 'AND', TRUE);
         }
       }
     }
