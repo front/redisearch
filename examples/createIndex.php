2272add5369aae831da6d1d808557f981c27c799
Foad Yousefi <foadyousefi@gmail.com>
2020-10-08 00:14:41 +0200

    Create example file for creating index

---
+++ examples/createIndex.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Create a temporary index which expires after 30 seconds
+ */
+require_once  '../vendor/autoload.php';
+
+use FKRediSearch\Index;
+use FKRediSearch\Setup;
+
+$client = Setup::connect();
+
+$index = new Index( $client );
+
+$index->setIndexName('idx');
+
+$index->on('HASH')
+      ->setPrefix('doc:')
+      ->setDefaultLang('norwegian')
+      ->setTemporary(30)
+      ->addTextField('title', 12)
+      ->addTextField('content')
+      ->addTextField('permalink')
+      ->addTagField('category')
+      ->addNumericField('date')
+      ->addGeoField('location')
+      ->create();
+
+echo 'Index have been created: ' . $index->getIndexName();
+
